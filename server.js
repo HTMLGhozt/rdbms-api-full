618bf243393c34ac563e4218dcbb3f4fa8cee3b4
Gary Dillard <thomas.dillard.studios@gmail.com>
2017-12-06 14:21:22 -0700

    added server;

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const sqlite = require('sqlite3');
+
+const routes = require('./api/routes.js');
+
+const server = express();
+server.use(bodyParser.json());
+
+routes(server);
+
+const port = 3030;
+server.listen(port, () => {
+  console.log('Server: ', port);
+});
